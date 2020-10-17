25eb7da2051f7aa1bb4b2e44d566acae31d4217f
Steven Araujo <steven.araujo@hotmail.com>
2020-10-17 02:45:47 -0500

    google analytics

--- src/components/seo.js
+++ src/components/seo.js
@@ -75,6 +75,17 @@ function SEO({ description, lang, meta }) {
       ].concat(meta)}
     >
       <link href={"https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&family=Inika:wght@400;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"} rel={"stylesheet"}/>
+      
+      {/*<!-- Global site tag (gtag.js) - Google Analytics -->*/}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180756855-1"></script>
+      <script>{`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-180756855-1');`
+      }</script>
+
+
     </Helmet>
   )
 }
