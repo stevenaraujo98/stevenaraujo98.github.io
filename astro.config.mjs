5b3d0baf64e3814286031633c56b1b455aadcde1
Steven Araujo <steven.araujo@hotmail.com>
2023-03-26 02:07:55 -0500

    edits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,24 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap(), tailwind()],
-  site: 'https://stevenaraujo98.github.io',
-  base: '/',
+  integrations: [
+    mdx(),
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en", // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: "en", // The `defaultLocale` value must present in `locales` keys
+          es: "es",
+        },
+      },
+    }),
+  ],
+  site: "https://stevenaraujo98.github.io",
+  base: "/",
 });
