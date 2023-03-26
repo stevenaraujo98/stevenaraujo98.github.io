0b361158e07d48e1196042354e4cba525e452e62
Steven Araujo <steven.araujo@hotmail.com>
2023-03-26 15:24:51 -0500

    astro-i18next

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    astroI18next(),
     mdx(),
     tailwind(),
     sitemap({
