1f857cbe794a344cd2cdcbdcdcc7e075e4c8fdab
Steven Araujo <steven.araujo@hotmail.com>
2023-03-27 02:45:22 -0500

    404 customized, banner fullstucks, i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     astroI18next(),
@@ -12,13 +15,16 @@ export default defineConfig({
     tailwind(),
     sitemap({
       i18n: {
-        defaultLocale: "en", // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        defaultLocale: "en",
+        // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
         locales: {
-          en: "en", // The `defaultLocale` value must present in `locales` keys
+          en: "en",
+          // The `defaultLocale` value must present in `locales` keys
           es: "es",
         },
       },
     }),
+    react(),
   ],
   site: "https://stevenaraujo98.github.io",
   base: "/",
